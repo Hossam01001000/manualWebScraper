92b602764813bb11ddb0f3d81714784d27eb36ac
HossamDaCyborg <hossamhamdy1482002@gmail.com>
2023-03-23 21:42:38 +0200

    probably fisnshed

---
+++ data.js
@@ -0,0 +1,9 @@
+let summary = 'skilled and dedicated Javascript developer, With a deep understanding of the language and a passion for creating efficient, elegant code, and a strong commitment to staying up-to-date with the latest industry trends and technologies, always looking for new ways to push the boundaries and exceed expectations. Whether working independently or as part of a team'
+let cover = `Dear Hiring Manager at Almuni,
+
+I am writing to apply for the Web Developer position currently available at your esteemed company. As a highly motivated and experienced web developer, I am confident in my ability to make a valuable contribution to your team.
+
+I'm familiar with client-side and server-side application technologies and serverless computing services 
+
+I also have a solid understanding of the programming paradigms, design patterns, algorithms and data structures.
+with excellent problem solving skills to come up with functional, efferent and clean solutions  for uncommon problems `
